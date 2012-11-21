7a42eb7a542943bcc6361a8049d24b339cf8a0a3
Tyler Blalock <twblalock@gmail.com>
2012-11-20 17:19:33 -0800

    Minor tweaks

--- time.sh
+++ time.sh
@@ -1,5 +1,8 @@
+echo -e "\nPython:"
 time python ./python/rss.py $1 > /dev/null
 
+echo -e "\nRuby:"
 time ruby ./ruby/rss.rb $1 > /dev/null
 
+echo -e "\nNode:"
 time node ./node/rss.js $1 > /dev/null
